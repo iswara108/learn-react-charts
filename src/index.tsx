0597fd6541a61447aeb04d7d3a8de4c2a0718707
iswara.chaitanya <iswara@sivananda.org>
2021-12-10 09:38:44 -0500

    simplify index

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,10 @@
-import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-//
 import Bar from "./Bar";
 
-const components = [["Bar", Bar]];
-
 export default function App() {
-  return (
-    <div>
-      {components.map(([label, Comp]) => {
-        return (
-          <div key={label + ""}>
-            <h1>{label}</h1>
-            <div>
-              <Comp />
-            </div>
-          </div>
-        );
-      })}
-      <div style={{ height: "50rem" }} />
-    </div>
-  );
+  return <Bar />;
 }
 
 const rootElement = document.getElementById("root");
