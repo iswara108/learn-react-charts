fa94c65456e92e2c7ef54286e68af70541faf049
iswara.chaitanya <iswara@sivananda.org>
2021-12-10 09:49:52 -0500

    organize data

--- src/getData.ts
+++ src/getData.ts
@@ -1,136 +1,124 @@
 export function getData() {
   return [
     {
-      label: "Series 1",
+      label: "Segment 1",
       data: [
         {
-          primary: "Ordinal Group 0",
-          secondary: 26,
-        },
-        {
-          primary: "Ordinal Group 1",
+          primary: "Day 1",
           secondary: 99,
         },
         {
-          primary: "Ordinal Group 2",
+          primary: "Day 2",
           secondary: 100,
         },
         {
-          primary: "Ordinal Group 3",
+          primary: "Day 3",
           secondary: 77,
         },
         {
-          primary: "Ordinal Group 4",
+          primary: "Day 4",
           secondary: 17,
         },
         {
-          primary: "Ordinal Group 5",
+          primary: "Day 5",
           secondary: 94,
         },
         {
-          primary: "Ordinal Group 6",
+          primary: "Day 6",
           secondary: 16,
         },
         {
-          primary: "Ordinal Group 7",
+          primary: "Day 7",
           secondary: 22,
         },
         {
-          primary: "Ordinal Group 8",
+          primary: "Day 8",
           secondary: 90,
         },
         {
-          primary: "Ordinal Group 9",
+          primary: "Day 9",
           secondary: 9,
         },
       ],
     },
     {
-      label: "Series 2",
+      label: "Segment 2",
       data: [
         {
-          primary: "Ordinal Group 0",
-          secondary: 43,
-        },
-        {
-          primary: "Ordinal Group 1",
+          primary: "Day 1",
           secondary: 78,
         },
         {
-          primary: "Ordinal Group 2",
+          primary: "Day 2",
           secondary: 1,
         },
         {
-          primary: "Ordinal Group 3",
+          primary: "Day 3",
           secondary: 3,
         },
         {
-          primary: "Ordinal Group 4",
+          primary: "Day 4",
           secondary: 26,
         },
         {
-          primary: "Ordinal Group 5",
+          primary: "Day 5",
           secondary: 62,
         },
         {
-          primary: "Ordinal Group 6",
+          primary: "Day 6",
           secondary: 67,
         },
         {
-          primary: "Ordinal Group 7",
+          primary: "Day 7",
           secondary: 22,
         },
         {
-          primary: "Ordinal Group 8",
+          primary: "Day 8",
           secondary: 13,
         },
         {
-          primary: "Ordinal Group 9",
+          primary: "Day 9",
           secondary: 85,
         },
       ],
     },
     {
-      label: "Series 3",
+      label: "Segment 3",
       data: [
         {
-          primary: "Ordinal Group 0",
-          secondary: 99,
-        },
-        {
-          primary: "Ordinal Group 1",
+          primary: "Day 1",
           secondary: 42,
         },
         {
-          primary: "Ordinal Group 2",
+          primary: "Day 2",
           secondary: 84,
         },
         {
-          primary: "Ordinal Group 3",
+          primary: "Day 3",
           secondary: 62,
         },
         {
-          primary: "Ordinal Group 4",
+          primary: "Day 4",
           secondary: 47,
         },
         {
-          primary: "Ordinal Group 5",
+          primary: "Day 5",
           secondary: 41,
         },
         {
-          primary: "Ordinal Group 6",
+          primary: "Day 6",
           secondary: 40,
         },
         {
-          primary: "Ordinal Group 7",
+          primary: "Day 7",
           secondary: 18,
         },
         {
-          primary: "Ordinal Group 8",
+          primary: "Day 8",
           secondary: 53,
         },
         {
-          primary: "Ordinal Group 9",
+          primary: "Day 9",
           secondary: 80,
         },
       ],
